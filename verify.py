6322b87303c1c8901e5030779f549037a41eddec
biles0026 <zacharybiles@gmail.com>
2020-12-05 00:03:16 -0600

    new report naming format

--- verify.py
+++ verify.py
@@ -4,26 +4,27 @@ import dns
 import dns.resolver as resolver
 import dns.query as query
 import csv
+from datetime import datetime
 
 filename = "dns.csv"
-
+cur_date = datetime.now().strftime("%m-%d-%Y")
+cur_time = datetime.now().strftime("%H-%M")
 dns_servers = ["8.8.8.8", "1.1.1.1"]
+report_filename = "report_" + cur_date + "_" + cur_time + ".csv"
 
-for dns_server in dns_servers:
-    resolver.override_system_resolver(resolver=dns_server)
 
-    report_filename = "report_" + dns_server + ".csv"
+with open(filename, 'r') as records:
+    # prep csv stuff
+    with open(report_filename, 'w') as report:
+        csvwriter = csv.writer(report)
+        fields = ['Record', 'Assumed Address',
+                  'Response Address', 'Type', 'DNS Server', 'Result']
+        csvwriter.writerow(fields)
 
-    with open(filename, 'r') as records:
-        # prep csv stuff
-        with open(report_filename, 'w') as report:
-            csvwriter = csv.writer(report)
-            fields = ['Record', 'Assumed Address',
-                      'Response Address', 'Type', 'DNS Server', 'Result']
-            csvwriter.writerow(fields)
-
-            for record in csv.reader(records):
-                # print(record)
+        for record in csv.reader(records):
+            for dns_server in dns_servers:
+                resolver.override_system_resolver(resolver=dns_server)
+            # print(record)
                 try:
                     answers = resolver.resolve(
                         qname=record[0], rdtype=record[2], search=dns_server)
